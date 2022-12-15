b961db52b8597e8206b863974d308df447e561b6
Carlo Santos <santos.carlo83@gmail.com>
2022-12-14 22:43:30 -0800

    1

--- app.js
+++ app.js
@@ -166,12 +166,10 @@ function ev_connected(status) {
 	},
 	set_mute: function (req, mode) {
 		if (mode == "on") {
-			control.mute(1);
-            mode = "off";
+			control.mute(0);
 		}	
 	    else if (mode == "off") {
 				control.mute(0);
-                mode = "on";
         }
 
 	    req.send_complete("Success");
